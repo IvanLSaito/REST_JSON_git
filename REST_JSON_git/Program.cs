91168bf3a2a284c63c6ad22334647cf7f8d07ef3
Iván López Saito <ivan.lopez.saito@gmail.com>
2018-01-25 11:19:35 +0100

    Pasar JSON a objetos en C#

--- REST_JSON_git/Program.cs
+++ REST_JSON_git/Program.cs
@@ -6,7 +6,19 @@ namespace REST_JSON_git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //fetch data (as JSON string)
+            var url = new Uri("http://localhost:1234/MyService.svc/json/4");
+            var client = new System.Net.WebClient();
+            var json = await client.DownloadStringTaskAsync(url);
+
+            //deserialize JSON into objects jjjjjjjj
+            var serializer = new JavaScriptSerializer();
+            var data = serializer.Deserialize<JSONSAMPLE.Data>(json);
+
+            //use the objects
+            Console.WriteLine(data.Numbre);
+            foreach (var item in data.Multiples)
+                Console.WriteLine("{0}, ", item);
         }
     }
 }
